879377296eff7ab10f4b41b3fb631a89b3d3ef7a
PTQuality <KONTAKT.TUNSKIPIOTR@GMAIL.COM>
2025-08-07 21:47:15 +0200

    Created new branch for course

--- pw-basics/tests/login.spec.ts
+++ pw-basics/tests/login.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from "@playwright/test";
 
-test("test", async ({ page }) => {
+test("login with correct credentials", async ({ page }) => {
   await page.goto("https://demo-bank.vercel.app/");
   await page.getByTestId("login-input").click();
   await page.getByTestId("login-input").fill("testerLO");
@@ -9,8 +9,20 @@ test("test", async ({ page }) => {
   await page.getByTestId("password-input").fill("123456789");
   await page.getByTestId("login-button").click();
 
-  await expect(page.getByTestId("user-name")).toHaveText("Jan DemobankowyX");
+  await expect(page.getByTestId("user-name")).toHaveText("Jan Demobankowy");
 });
 
+test("login with incorrect credentials with incorrect username", async ({
+  page,
+}) => {
+  await page.goto("https://demo-bank.vercel.app/");
+  await page.getByTestId("login-input").click();
+  await page.getByTestId("login-input").fill("tester");
+  await page.getByTestId("login-input").press("Tab");
+  await page.getByTestId("password-input").click();
+  await page.getByTestId("error-login-id").click();
 
-
+  await expect(page.getByTestId("error-login-id")).toHaveText(
+    "identyfikator ma min. 8 znaków"
+  );
+});
