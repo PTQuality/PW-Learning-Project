005c6ebf9809fb423e039ae35f14788585de5b3a
PTQuality <KONTAKT.TUNSKIPIOTR@GMAIL.COM>
2025-08-09 11:14:03 +0200

    First AAA pattern implementation

--- pw-basics/tests/login.spec.ts
+++ pw-basics/tests/login.spec.ts
@@ -1,37 +1,36 @@
 import { test, expect } from "@playwright/test";
 test.describe("User login to Demobank", () => {
   test("login with correct credentials", async ({ page }) => {
-    await page.goto("https://demo-bank.vercel.app/");
-    await page.getByTestId("login-input").click();
-    await page.getByTestId("login-input").fill("testerLO");
-    await page.getByTestId("login-input").press("Tab");
-    await page.getByTestId("password-input").click();
-    await page.getByTestId("password-input").fill("123456789");
+    // Arrange
+    const url = "https://demo-bank.vercel.app/";
+    const userId = "testerLO";
+    const userPassword = "123456789";
+    const expectedUserName = "Jan Demobankowy";
+
+    //Act
+    await page.goto(url);
+    await page.getByTestId("login-input").fill(userId);
+    await page.getByTestId("password-input").fill(userPassword);
     await page.getByTestId("login-button").click();
 
-    await expect(page.getByTestId("user-name")).toHaveText("Jan Demobankowy");
+    //Assert
+    await expect(page.getByTestId("user-name")).toHaveText(expectedUserName);
   });
 
-  test("login with incorrect credentials with incorrect username", async ({
-    page,
-  }) => {
+  test("unsuccessful login with too short login", async ({ page }) => {
     await page.goto("https://demo-bank.vercel.app/");
-    await page.getByTestId("login-input").click();
     await page.getByTestId("login-input").fill("tester");
     await page.getByTestId("login-input").press("Tab");
     await page.getByTestId("password-input").click();
-    await page.getByTestId("error-login-id").click();
 
     await expect(page.getByTestId("error-login-id")).toHaveText(
       "identyfikator ma min. 8 znaków"
     );
   });
 
-  test.only("unsuccessful login with too short password", async ({ page }) => {
+  test("unsuccessful login with too short password", async ({ page }) => {
     await page.goto("https://demo-bank.vercel.app/");
-    await page.getByTestId("login-input").click();
     await page.getByTestId("login-input").fill("testerLO");
-    await page.getByTestId("password-input").click();
     await page.getByTestId("password-input").fill("1234");
     await page.getByTestId("password-input").blur();
 
